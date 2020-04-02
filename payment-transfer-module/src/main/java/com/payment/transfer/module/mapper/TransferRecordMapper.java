9520519d7604d057b3572d67305293ab2f74a13e
Bing ZHANG <bing@Bings-MacBook-Pro.local>
2020-04-01 21:29:42 -0400

    add trnasferRecordmapper

---
+++ payment-transfer-module/src/main/java/com/payment/transfer/module/mapper/TransferRecordMapper.java
@@ -0,0 +1,21 @@
+package com.payment.transfer.module.mapper;
+
+import com.payment.transfer.module.entity.TransferRecord;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+@Mapper
+public interface TransferRecordMapper {
+
+    @Select("SELECT * FROM transfer_record WHERE message_id = #{messageId}")
+    public TransferRecord findTransferRecord(String messageId);
+
+    @Insert("INSERT INTO transfer_record (message_id, user_id, amount, status) VALUES (#{messageId}, #{userId}, #{amount}, #{status})")
+    public void addNewTransferRecord(TransferRecord transferRecord);
+
+    @Update("UPDATE transfer_record SET status = #{status} WHERE message_id = #{messageId}")
+    public void updateTransferRecord(TransferRecord transferRecord);
+
+}
--- payment-transfer-module/src/test/java/com/payment/transfer/module/mapper/AccountMapperTest.java
+++ payment-transfer-module/src/test/java/com/payment/transfer/module/mapper/AccountMapperTest.java
@@ -1,6 +1,8 @@
 package com.payment.transfer.module.mapper;
 
 import com.payment.transfer.module.entity.Account;
+import com.payment.transfer.module.entity.TransferRecord;
+import org.apache.ibatis.annotations.Mapper;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -23,15 +25,20 @@ public class AccountMapperTest {
     @Resource
     private AccountMapper accountMapper;
 
+    @Resource
+    private TransferRecordMapper transferRecordMapper;
+
     @Before
     public void setup() {
         Assert.assertNotNull(accountMapper);
+        Assert.assertNotNull(transferRecordMapper);
     }
 
     @Test
     public void accountAddTest() {
         Account account = Account.builder().userId(UUID.randomUUID().toString()).amount(1000).updateTime(Timestamp.valueOf(LocalDateTime.now())).build();
         accountMapper.AddNewAccount(account);
+
     }
 
     @Test
@@ -39,6 +46,22 @@ public class AccountMapperTest {
         Account account = accountMapper.getAccountInfo("c0eb0353-5477-44ed-8cde-4bd56552e411");
         Assert.assertNotNull(account);
         Assert.assertEquals(1000, (long)account.getAmount());
+    }
+
+    @Test
+    public void transferRecordTest() {
+        String messageID = UUID.randomUUID().toString();
+        TransferRecord transferRecord = TransferRecord.builder()
+                .messageId(messageID)
+                .userId("c0eb0353-5477-44ed-8cde-4bd56552e411")
+                .status("PENDING")
+                .amount(100)
+                .build();
+        transferRecordMapper.addNewTransferRecord(transferRecord);
+        TransferRecord transferRecord1 = transferRecordMapper.findTransferRecord(messageID);
+        transferRecord1.setStatus("CONFIRMED");
+        transferRecordMapper.updateTransferRecord(transferRecord1);
+
     }
 
 }
